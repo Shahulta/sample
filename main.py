fe2aae8e214b64a3a50ce0fb8592ac5e30b3df1e
Shahulta <131171791+Shahulta@users.noreply.github.com>
2023-04-25 00:29:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 API_ID = "17296819"
 API_HASH = "baa838bcf579f71dbcd5ef2ab9ca8f2b"
-BOT_TOKEN = "5928195468:AAFpH1azW43J3-blOLah2M_y677a6BgrSe0"
+BOT_TOKEN ="5998095303:AAElI1_jxNpH20FchT6TQnupX-0jLvbpHuQ")
 
 
 BOT = Client(
@@ -11,6 +11,7 @@ BOT = Client(
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
+
 
 
 @BOT.on_message(filters.command("start"))
