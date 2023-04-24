876913bd853986b177a5389a01475077fa521723
Shahulta <131171791+Shahulta@users.noreply.github.com>
2023-04-24 23:15:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from pyrogram import Client, filters
 
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = "17296819"
+API_HASH = "baa838bcf579f71dbcd5ef2ab9ca8f2b"
+BOT_TOKEN = "5998095303:AAFDVVJBRQXKp5vyuo7uuwCp1fTsWZtFaRw"
+
+
+BOT = Client(
+    name="PyrogramBot"
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+
+print("BOT STARTED")
+
+
+BOT.run()
+
+
