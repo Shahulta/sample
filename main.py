27e38ef2b7e4a7bc8df33964c2a211d29446bb69
Shahulta <131171791+Shahulta@users.noreply.github.com>
2023-04-25 08:24:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 API_ID = "17296819"
 API_HASH = "baa838bcf579f71dbcd5ef2ab9ca8f2b"
-BOT_TOKEN ="5998095303:AAElI1_jxNpH20FchT6TQnupX-0jLvbpHuQ")
+BOT_TOKEN ="5998095303:AAElI1_jxNpH20FchT6TQnupX-0jLvbpHuQ"
 
 
 BOT = Client(
@@ -16,7 +16,10 @@ BOT = Client(
 
 @BOT.on_message(filters.command("start"))
 async def start_cmd(client, message):
-  await message.reply_text("Hallo! How are you, \n Iam a bot ")
+  await message.reply_photo(
+    photo="https://telegra.ph/file/8ac514e8c71ec61a9a051.jpg",
+    caption="Helo! \nHow are you? \n\n\nplease click /about"
+  )
 
 @BOT.on_message(filters.command("about"))
 async def about_cmd(client, message):
